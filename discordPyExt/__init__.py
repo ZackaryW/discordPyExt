be9f58cf4f5aa7262c139be233dc4c853ec158b6
ZackaryW <zackaryxcwang@gmail.com>
2022-10-30 15:52:34 -0400

    env: update ignore

--- discordPyExt/__init__.py
+++ discordPyExt/__init__.py
@@ -2,3 +2,12 @@ from discordPyExt.components.botx import BotX
 from discordPyExt.components.embedX import EmbedX, EmbedFactory
 from discordPyExt.components.text import Text, TextMappingTypes, ImmutableText
 from discordPyExt.components.ctxx import Ctxl, Ctxx
+
+
+__all__ = [
+    "components",
+    "ext",
+    "interfaces",
+    "utils",
+    "setup",
+]

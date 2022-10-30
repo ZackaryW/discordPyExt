807b0113ebee2f973bf2bbcebb27b2c872f1f971
ZackaryW <zackaryxcwang@gmail.com>
2022-10-30 15:37:11 -0400

    Update test_pkg_setup.py

--- tests/test_pkg_setup.py
+++ tests/test_pkg_setup.py
@@ -2,7 +2,7 @@
 import shutil
 from discordPyExt.ext.testCase import TestCaseX
 from discordPyExt.setup import DcDeployer
-from discordPyExt.setup.ext import DeployFlask, DeployReplitFlaskColdTrigger
+from discordPyExt.setup.ext import DeployFlask, DeployReplitFlaskColdTrigger, DeployFlaskSQLAlchemy
 
 class t_setup(TestCaseX):
     def test_can_deploy(self):
@@ -11,7 +11,7 @@ class t_setup(TestCaseX):
         """
         
         dp = DcDeployer(
-            extensions=[DeployFlask],
+            extensions=[DeployFlask, DeployReplitFlaskColdTrigger, DeployFlaskSQLAlchemy],
             path="test_data/setup",
             config_path="test_data/config",
             setup_mode=True,
@@ -30,9 +30,11 @@ class t_setup(TestCaseX):
             json.dump(json_data, f)    
     
         dp = DcDeployer(
-            extensions=[DeployFlask, DeployReplitFlaskColdTrigger],
+            extensions=[DeployFlask, DeployReplitFlaskColdTrigger, DeployFlaskSQLAlchemy],
             path="test_data/setup",
             config_path="test_data/config",
+            # in memory
+            SQLALCHEMY_DATABASE_URI = "sqlite://",
         )
         
         driver = dp.config.getDriver("DISCORD_TOKEN")
